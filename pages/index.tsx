45e7cb4858ab8a41581b0567c03b3dd92d0f0cdd
Peter Georgas <peter_georgas@yahoo.com>
2022-03-12 22:30:00 -0500

    Deleted unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { getWordOfTheDay } from "../word-util/util";
 
